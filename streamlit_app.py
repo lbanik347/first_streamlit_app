5fcd26b356466754731f6cae30819052ffeb00b6
lbanik347 <123911726+lbanik347@users.noreply.github.com>
2023-01-31 00:57:56 -0800

    Update streamlit_app.py
    Code to read data of 20 fruits using pandas, is added.

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,7 @@ streamlit.text('🥬 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑 🍞 Avocado Toast')
 streamlit.header('🍌 🍎 Build Your Own Fruit Smoothie 🥝 🍇')
+
+import pandas
+my_fruit_list = pandas.read_csv(“https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt”)
+streamlit.dataframe(my_fruit_list)
