b5c64aaed97f99d78c8fc6b1bd9f7890e4296fcd
lbanik347 <123911726+lbanik347@users.noreply.github.com>
2023-02-14 12:32:45 -0800

    Delete line 26 i.e. output json response in text format

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,6 @@ streamlit.dataframe(fruits_to_show)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text(fruityvice_response.json())
 
 # Normalize JSON response in table form
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
